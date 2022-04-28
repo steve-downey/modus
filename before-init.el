4b33af2af0f6d0969ae35004a2de31d9c3e2d42f
Steve Downey <sdowney2@bloomberg.net>
2022-04-28 16:46:04 -0400

    remove themes that have been loaded before loading a new one

--- before-init.el
+++ before-init.el
@@ -1,1 +1,5 @@
 ;; load before processing rest of init.el
+
+;; remove themes that have been loaded before loading a new one
+(defadvice load-theme (before theme-dont-propagate activate)
+  (mapcar #'disable-theme custom-enabled-themes))
