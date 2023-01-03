c5160060a1262c8e8e83dda5907c16517f41fd67
Steve Downey <sdowney@gmail.com>
2023-01-02 21:20:18 -0500

    Updates for modus themes 4.0
    A number of incompatible changes in configuration

--- after-init.el
+++ after-init.el
@@ -1,6 +1,5 @@
 ;; load after processing init.el
 
-;;; For packaged versions which must use `require':
 (use-package modus-themes
   :ensure
   :init
@@ -8,65 +7,8 @@
   (setq modus-themes-italic-constructs t
         modus-themes-bold-constructs t
         modus-themes-mixed-fonts t
-        modus-themes-subtle-line-numbers t
-        modus-themes-intense-mouseovers nil
-        modus-themes-deuteranopia nil
-        modus-themes-tabs-accented t
         modus-themes-variable-pitch-ui t
-        modus-themes-inhibit-reload t ; only applies to `customize-set-variable' and related
-
-        modus-themes-fringes 'intense ; {nil,'subtle,'intense}
-
-        ;; Options for `modus-themes-lang-checkers' are either nil (the
-        ;; default), or a list of properties that may include any of those
-        ;; symbols: `straight-underline', `text-also', `background',
-        ;; `intense' OR `faint'.
-        modus-themes-lang-checkers '(intense)
-
-        ;; Options for `modus-themes-mode-line' are either nil, or a list
-        ;; that can combine any of `3d' OR `moody', `borderless',
-        ;; `accented', a natural number for extra padding (or a cons cell
-        ;; of padding and NATNUM), and a floating point for the height of
-        ;; the text relative to the base font size (or a cons cell of
-        ;; height and FLOAT)
-        modus-themes-mode-line '(accented borderless (padding . 4) (height . 0.9))
-
-        ;; Same as above:
-        ;; modus-themes-mode-line '(accented borderless 4 0.9)
-
-        ;; Options for `modus-themes-markup' are either nil, or a list
-        ;; that can combine any of `bold', `italic', `background',
-        ;; `intense'.
-        modus-themes-markup '(background italic)
-
-        ;; Options for `modus-themes-syntax' are either nil (the default),
-        ;; or a list of properties that may include any of those symbols:
-        ;; `faint', `yellow-comments', `green-strings', `alt-syntax'
-        modus-themes-syntax `(alt-syntax)
-
-        ;; Options for `modus-themes-hl-line' are either nil (the default),
-        ;; or a list of properties that may include any of those symbols:
-        ;; `accented', `underline', `intense'
-        modus-themes-hl-line '(underline)
-
-        ;; Options for `modus-themes-paren-match' are either nil (the
-        ;; default), or a list of properties that may include any of those
-        ;; symbols: `bold', `intense', `underline'
-        modus-themes-paren-match '(bold intense)
-
-        ;; Options for `modus-themes-links' are either nil (the default),
-        ;; or a list of properties that may include any of those symbols:
-        ;; `neutral-underline' OR `no-underline', `faint' OR `no-color',
-        ;; `bold', `italic', `background'
-        modus-themes-links '(neutral-underline background)
-
-        ;; Options for `modus-themes-box-buttons' are either nil (the
-        ;; default), or a list that can combine any of `flat', `accented',
-        ;; `faint', `variable-pitch', `underline', `all-buttons', the
-        ;; symbol of any font weight as listed in `modus-themes-weights',
-        ;; and a floating point number (e.g. 0.9) for the height of the
-        ;; button's text.
-        modus-themes-box-buttons '(variable-pitch flat faint 0.9)
+        modus-themes-custom-auto-reload t
 
         ;; Options for `modus-themes-prompts' are either nil (the
         ;; default), or a list of properties that may include any of those
@@ -90,33 +32,46 @@
                                    (selection . (semibold accented))
                                    (popup . (accented intense)))
 
-        modus-themes-mail-citations nil ; {nil,'intense,'faint,'monochrome}
-
-        ;; Options for `modus-themes-region' are either nil (the default),
-        ;; or a list of properties that may include any of those symbols:
-        ;; `no-extend', `bg-only', `accented'
-        modus-themes-region '(bg-only no-extend)
-
-        ;; Options for `modus-themes-diffs': nil, 'desaturated, 'bg-only
-        modus-themes-diffs 'desaturated
 
         modus-themes-org-blocks 'gray-background ; {nil,'gray-background,'tinted-background}
 
-        modus-themes-org-agenda ; this is an alist: read the manual or its doc string
-        '((header-block . (variable-pitch 1.3))
-          (header-date . (grayscale workaholic bold-today 1.1))
-          (event . (accented varied))
-          (scheduled . uniform)
-          (habit . traffic-light))
 
         modus-themes-headings ; this is an alist: read the manual or its doc string
         '((1 . (overline background variable-pitch 1.3))
           (2 . (rainbow overline 1.1))
           (t . (semibold))))
 
-  ;; Load the theme files before enabling a theme
-  (modus-themes-load-themes)
+  (setq modus-themes-common-palette-overrides
+        modus-themes-preset-overrides-intense)
+
+  ;; Subtle blue background, neutral foreground, intense blue border
+  (setq modus-themes-common-palette-overrides
+        '((bg-mode-line-active bg-blue-subtle)
+          (fg-mode-line-active fg-main)
+          (border-mode-line-active blue-intense)))
+
+  ;; Make the fringe colorful, but nuanced
+  (setq modus-themes-common-palette-overrides
+        '((fringe bg-blue-nuanced)))
+
+  ;; Enable underlines by applying a color to them
+  (setq modus-themes-common-palette-overrides
+        '((bg-paren-match bg-magenta-intense)
+          (underline-paren-match fg-main)))
+
+
+  ;; Make line numbers less intense
+  (setq modus-themes-common-palette-overrides
+        '((fg-line-number-inactive "gray50")
+          (fg-line-number-active fg-main)
+          (bg-line-number-inactive unspecified)
+          (bg-line-number-active unspecified)))
+
+  (setq modus-themes-to-toggle
+        '(modus-operandi-tinted modus-vivendi-tinted))
   :config
-  ;; Load the theme of your choice:
-  (modus-themes-load-vivendi) ;; OR (modus-themes-load-vivendi)
+  ;; Load the theme of your choice.
+  (load-theme 'modus-vivendi-tinted :no-confim)
   :bind ("<f5>" . modus-themes-toggle))
+
+;;; after-init.el ends here
