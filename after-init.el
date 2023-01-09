40c909c618e7f40756e279212390e1c655d91238
Steve Downey <sdowney2@bloomberg.net>
2023-01-09 09:36:24 -0500

    Remove no longer working Modus config options

--- after-init.el
+++ after-init.el
@@ -9,69 +9,68 @@
         modus-themes-mixed-fonts t
         modus-themes-variable-pitch-ui t
         modus-themes-custom-auto-reload t
+        modus-themes-disable-other-themes t
 
         ;; Options for `modus-themes-prompts' are either nil (the
         ;; default), or a list of properties that may include any of those
-        ;; symbols: `background', `bold', `gray', `intense', `italic'
-        modus-themes-prompts '(intense bold)
+        ;; symbols: `italic', `WEIGHT'
+        modus-themes-prompts '(bold)
 
-        ;; The `modus-themes-completions' is an alist that reads three
-        ;; keys: `matches', `selection', `popup'.  Each accepts a nil
-        ;; value (or empty list) or a list of properties that can include
-        ;; any of the following (for WEIGHT read further below):
+        ;; The `modus-themes-completions' is an alist that reads two
+        ;; keys: `matches', `selection'.  Each accepts a nil value (or
+        ;; empty list) or a list of properties that can include any of
+        ;; the following (for WEIGHT read further below):
         ;;
-        ;; `matches' - `background', `intense', `underline', `italic', WEIGHT
-        ;; `selection' - `accented', `intense', `underline', `italic', `text-also' WEIGHT
-        ;; `popup' - same as `selected'
-        ;; `t' - applies to any key not explicitly referenced (check docs)
-        ;;
-        ;; WEIGHT is a symbol such as `semibold', `light', or anything
-        ;; covered in `modus-themes-weights'.  Bold is used in the absence
-        ;; of an explicit WEIGHT.
+        ;; `matches'   :: `underline', `italic', `WEIGHT'
+        ;; `selection' :: `underline', `italic', `WEIGHT'
         modus-themes-completions '((matches . (extrabold))
-                                   (selection . (semibold accented))
-                                   (popup . (accented intense)))
+                                   (selection . (semibold italic)))
 
 
         modus-themes-org-blocks 'gray-background ; {nil,'gray-background,'tinted-background}
 
 
-        modus-themes-headings ; this is an alist: read the manual or its doc string
-        '((1 . (overline background variable-pitch 1.3))
-          (2 . (rainbow overline 1.1))
-          (t . (semibold))))
-
-  (setq modus-themes-common-palette-overrides
-        modus-themes-preset-overrides-intense)
+        ;; The `modus-themes-headings' is an alist: read the manual's
+        ;; node about it or its doc string.  Basically, it supports
+        ;; per-level configurations for the optional use of
+        ;; `variable-pitch' typography, a height value as a multiple of
+        ;; the base font size (e.g. 1.5), and a `WEIGHT'
+        modus-themes-headings
+        '((1 . (variable-pitch 1.5))
+          (2 . (1.3))
+          (agenda-date . (1.3))
+          (agenda-structure . (variable-pitch light 1.8))
+          (t . (1.1))))
 
   ;; Subtle blue background, neutral foreground, intense blue border
-  (setq modus-themes-common-palette-overrides
-        '((bg-mode-line-active bg-blue-subtle)
-          (fg-mode-line-active fg-main)
-          (border-mode-line-active blue-intense)))
+  (customize-set-variable 'modus-themes-common-palette-overrides
+                          '((bg-mode-line-active bg-blue-subtle)
+                            (fg-mode-line-active fg-main)
+                            (border-mode-line-active blue-intense)))
 
   ;; Make the fringe colorful, but nuanced
-  (setq modus-themes-common-palette-overrides
-        '((fringe bg-blue-nuanced)))
+  (customize-set-variable 'modus-themes-common-palette-overrides
+                          '((fringe bg-blue-nuanced)))
 
   ;; Enable underlines by applying a color to them
-  (setq modus-themes-common-palette-overrides
-        '((bg-paren-match bg-magenta-intense)
-          (underline-paren-match fg-main)))
+  (customize-set-variable 'modus-themes-common-palette-overrides
+                          '((bg-paren-match bg-magenta-intense)
+                            (underline-paren-match fg-main)))
 
 
   ;; Make line numbers less intense
-  (setq modus-themes-common-palette-overrides
-        '((fg-line-number-inactive "gray50")
-          (fg-line-number-active fg-main)
-          (bg-line-number-inactive unspecified)
-          (bg-line-number-active unspecified)))
+  (customize-set-variable 'modus-themes-common-palette-overrides
+                          '((fg-line-number-inactive "gray50")
+                            (fg-line-number-active fg-main)
+                            (bg-line-number-inactive unspecified)
+                            (bg-line-number-active unspecified)))
 
   (setq modus-themes-to-toggle
-        '(modus-operandi-tinted modus-vivendi-tinted))
+        '(modus-vivendi-tinted modus-operandi-tinted))
   :config
   ;; Load the theme of your choice.
   (load-theme 'modus-vivendi-tinted :no-confim)
+
   :bind ("<f5>" . modus-themes-toggle))
 
 ;;; after-init.el ends here
